1ab5cb8b7f8347d428eaf588c94e489c4834dd13
soulyana <soulakew@umd.edu>
2018-05-10 11:53:27 -0400

    created all the news categories on navbar and html templates for them

--- src/main/java/me/soulyana/springsecuritybasic/MainController.java
+++ src/main/java/me/soulyana/springsecuritybasic/MainController.java
@@ -25,13 +25,49 @@ public class MainController {
     }
 
     @RequestMapping("/business")
-    public String showContact() {
-        return "contact";
+    public String showBusiness(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        News news = restTemplate.getForObject("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=595fb788918a4fdcb687e51bcd5b06f7", News.class);
+        model.addAttribute("articles", news.getArticles());
+        return "business";
     }
 
-    @RequestMapping("/post")
-    public String showPost() {
-        return "entertainment";
+    @RequestMapping("/general")
+    public String showGeneral(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        News news = restTemplate.getForObject("https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=595fb788918a4fdcb687e51bcd5b06f7", News.class);
+        model.addAttribute("articles", news.getArticles());
+        return "general";
     }
 
+    @RequestMapping("/health")
+    public String showHealth(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        News news = restTemplate.getForObject("https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=595fb788918a4fdcb687e51bcd5b06f7", News.class);
+        model.addAttribute("articles", news.getArticles());
+        return "health";
+    }
+
+    @RequestMapping("/science")
+    public String showScience(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        News news = restTemplate.getForObject("https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=595fb788918a4fdcb687e51bcd5b06f7", News.class);
+        model.addAttribute("articles", news.getArticles());
+        return "science";
+    }
+    @RequestMapping("/sports")
+    public String showSports(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        News news = restTemplate.getForObject("https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=595fb788918a4fdcb687e51bcd5b06f7", News.class);
+        model.addAttribute("articles", news.getArticles());
+        return "sports";
+    }
+
+    @RequestMapping("/technology")
+    public String showTechnology(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        News news = restTemplate.getForObject("https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=595fb788918a4fdcb687e51bcd5b06f7", News.class);
+        model.addAttribute("articles", news.getArticles());
+        return "technology";
+    }
 }
